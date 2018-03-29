3911580e20457df1d74b5675b2b6d812a744516a
Hubert Hirsch <hubert.hirsch@paysafe.com>
2018-03-30 00:18:43 +0200

    lemonbar option to adapt to monitor changes

--- .i3/i3-lemon-VIE-HUBHIR-MOB/i3_lemonbar.sh
+++ .i3/i3-lemon-VIE-HUBHIR-MOB/i3_lemonbar.sh
@@ -18,16 +18,7 @@ trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
 mkfifo "${panel_fifo}"
 
 ### monitors
-### connected and active, will contain string like " connected 1920x..."
-### formatted as {left-x-coord}={output-name}, sorted by x-coord, output-names joined as whitespace csv
-### in the end monitors is contains output-names separated by whitespaces, in the order that lemonbar understands
-### i.e. output index 0 = lemonbar %{S0}, output index 1 = %{S1}, etc.
-monitors=$(xrandr \
-    | awk '/[0-9]+x[0-9]+\+[0-9]+\+[0-9]+/ {resolutionAndPosition=($3!="primary") ? $3 : $4; print gensub(/^[^\+]+\+([0-9]+)\+.*$/, "\\1", "g", resolutionAndPosition)"="$1}' \
-    | sort -t '=' -k 1 -n \
-    | awk -F '=' '{print $2}' \
-    | paste -sd " ")
-printf "MON%s\n" "${monitors}" > "${panel_fifo}" &
+$(dirname $0)/i3_lemonbar_monitors.sh > "${panel_fifo}" &
 
 ## Window title, "WIN"
 xprop -spy -root _NET_ACTIVE_WINDOW | sed -un 's/.*\(0x.*\)/WIN\1/p' > "${panel_fifo}" &
@@ -104,7 +95,8 @@ done &
 
 #### LOOP FIFO
 
-cat "${panel_fifo}" | $(dirname $0)/i3_lemonbar_parser.sh \
+cat "${panel_fifo}" \
+    | $(dirname $0)/i3_lemonbar_parser.sh \
 	| lemonbar -b -p -f "${font}" -f "${iconfont}" -g "${geometry}" -B "${color_back}" -F "${color_fore}" -U "${color_underline}" -u "${pixels_underline}" &
 
 wait
